9abcd5473ed12472f7356a7c3b535330eb50bc53
Stephen Nelson-Smith <stephen.nelsonsmith@ticketmaster.co.uk>
2024-11-30 17:02:26 +0000

    New: Initial project setup and starter README

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Clojure development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    emacs-overlay.url = "github:nix-community/emacs-overlay";
+    
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, emacs-overlay, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ emacs-overlay.overlay ];
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            temurin-bin-21
+            clojure
+            clojure-lsp
+            
+            ((emacsPackagesFor emacs29).emacsWithPackages (epkgs: [
+              epkgs.cider
+              epkgs.clojure-mode
+              epkgs.paredit
+              epkgs.rainbow-delimiters
+	      epkgs.magit
+            ]))
+          ];
+        };
+      });
+}
